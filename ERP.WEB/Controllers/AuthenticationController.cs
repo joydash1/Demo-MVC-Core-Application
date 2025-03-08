9f013858db4fa2ad6553fa794900c057e60b7b12
Nir jhor <nirjhordas2020@gmail.com>
2025-03-08 14:40:06 +0600

    Structure Complete

---
+++ ERP.WEB/Controllers/AuthenticationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.WEB.Controllers
+{
+
+    public class AuthenticationController : Controller
+    {
+        public ActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
--- ERP.WEB/Program.cs
+++ ERP.WEB/Program.cs
@@ -5,11 +5,9 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -24,6 +22,4 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
-
 app.Run();
