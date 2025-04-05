3109806a94e6ddd1973d14aef9db5eb1762b01d9
Nir jhor <nirjhordas2020@gmail.com>
2025-04-05 11:02:30 +0600

    Done

---
+++ ERP.Utility/Helpers/WhatsAppService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace ERP.Utility.Helpers
+{
+    public static class WhatsAppService
+    {
+        private static readonly string accountSid = "US8736b7c430801e18006d74f97ad778ec";
+        private static readonly string authToken = "5FLWACLUPHWK5KRM4XBCS34A";
+        private static readonly string fromWhatsAppNumber = "whatsapp:+14155238886";
+
+        public static void SendWhatsAppMessage(string clientId, string clientPhoneNumber)
+        {
+            TwilioClient.Init(accountSid, authToken);
+
+            var to = new PhoneNumber($"whatsapp:{clientPhoneNumber}");
+
+            var message = MessageResource.Create(
+                to: to,
+                from: new PhoneNumber(fromWhatsAppNumber),
+                body: $"Hello! This is a message for client ID: {clientId}"
+            );
+        }
+    }
+}
