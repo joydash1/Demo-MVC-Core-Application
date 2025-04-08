9554aeb2233e599f792d68e0861d96b069be9f2f
Nir jhor <nirjhor268@gmail.com>
2025-04-08 21:18:52 +0600

    Done

--- ERP.WEB/Controllers/ProductStockController.cs
+++ ERP.WEB/Controllers/ProductStockController.cs
@@ -1,5 +1,8 @@
-﻿using ERP.Infrastructure.Interfaces;
+﻿using ERP.DataAccess.DTOs.LC_Open;
+using ERP.Infrastructure.Interfaces;
+using ERP.Utility.Helpers;
 using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ERP.WEB.Controllers
 {
@@ -15,10 +18,37 @@ namespace ERP.WEB.Controllers
         }
 
         #region Stock
-        public IActionResult Stock()
+        public async Task<IActionResult> Stock()
         {
+            ViewBag.LCFileList = await _spService.GetDataWithoutParameterAsync<LCTransactionListDto>("USP_GET_LC_OPENING_TRANSACTION_LIST").ToListAsync();
+
             return View();
-        } 
+        }
+        [HttpPost]
+        public async Task<IActionResult> GetLCTransactionInfoById(int Id)
+        {
+            try
+            {
+                var data = await _spService.GetDataWithParameterAsync<LCTransactionListDto>(new
+                {
+                    ID = Id
+                }, "USP_GET_LC_OPENING_TRANSACTION_LIST");
+
+                return Json(new
+                {
+                    Status = true,
+                    Data = data
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    Status = false,
+                    Message = ex.GetBaseException()
+                });
+            }
+        }
         #endregion
     }
 }
