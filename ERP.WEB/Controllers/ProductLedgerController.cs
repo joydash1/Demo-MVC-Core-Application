8559b8f8375e27d500748b27e9407abfa8c263ed
Nir jhor <nirjhor268@gmail.com>
2025-06-10 00:28:52 +0600

    Done

---
+++ ERP.WEB/Controllers/ProductLedgerController.cs
@@ -0,0 +1,22 @@
+﻿using ERP.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.WEB.Controllers
+{
+    public class ProductLedgerController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ISpService _spService;
+
+        public ProductLedgerController(IUnitOfWork unitOfWork, ISpService spService)
+        {
+            _unitOfWork = unitOfWork;
+            _spService = spService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
